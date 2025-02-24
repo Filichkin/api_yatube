db5b6dbd3e309c7b4e38f62232e2523659479532
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-24 11:27:29 +0300

    add serializers.py

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from posts.models import Group, Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'autor', 'post', 'text', 'created')
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,10 +2,18 @@ from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import SimpleRouter
 
+from . import views
 
 v1_router = SimpleRouter()
-
+v1_router.register(r'posts', views.PostViewSet)
+v1_router.register(r'groups', views.GroupViewSet)
+v1_router.register(
+    r'^posts/(?P<post_id>\d+)/comments',
+    views.CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/api-token-auth/', obtain_auth_token),
+    path('v1/', include(v1_router.urls)),
 ]
