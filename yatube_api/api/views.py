384667a33162cc9f4713ea197bae06631c849675
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-24 13:46:13 +0300

    add PostViewSet, CommentViewSet, GroupViewSet and IsOwnerOrReadOnly

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from posts.models import Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.post_comments.all()
+
+    def perform_create(self, serializer):
+        post = self.get_post()
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
